52c5b2f4807846b614f09ab9862bdcc26489e963
Nilay Sheth <nilay_994@hotmail.com>
2023-12-14 22:37:54 +0530

    [legopi] start led on rust

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,21 +1,55 @@
+// @file: basic.rs: LED control for LegoPi
+
+// for main loop
+use std::{thread, time};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+// WS2812 related
 use rs_ws281x::ControllerBuilder;
 use rs_ws281x::ChannelBuilder;
 use rs_ws281x::StripType;
 
-fn main() {
+// math
+use core::f32::consts::PI;
+
+fn strobe()
+{
+
+}
+
+fn pulse()
+{
+
+}
+
+fn main()
+{
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
+    println!("Program started, press Ctrl-C for exit");
+
+    // thread/sleep related
+    let ten_millis = time::Duration::from_millis(10);
+    let mut millis_elapsed: u32 = 0;
+
     // Construct a single channel controller. Note that the
     // Controller is initialized by default and is cleaned up on drop
-
     let mut controller = ControllerBuilder::new()
         .freq(800_000)
         .dma(10)
         .channel(
             0, // Channel Index
             ChannelBuilder::new()
-                .pin(10) // GPIO 10 = SPI0 MOSI
-                .count(64) // Number of LEDs
+                .pin(18) // GPIO 10 = SPI0 MOSI
+                .count(8) // Number of LEDs
                 .strip_type(StripType::Ws2812)
-                .brightness(20) // default: 255
+                .brightness(0) // default: 255
                 .build(),
         )
         .build()
@@ -24,8 +58,26 @@ fn main() {
     let leds = controller.leds_mut(0);
 
     for led in leds {
-        *led = [0, 0, 255, 0];
+        *led = [0x14, 0xFF, 0x39, 100];
     }
 
+    while running.load(Ordering::SeqCst) {
+        // sinewave, amplitude of 100, min-max: 0 to 200, time period of 3000
+        let brightness = 125.0 + 125.0 * (2.0 * PI/ 3000.0 * millis_elapsed as f32).sin();
+        println!("brightness: {}", brightness as u8);
+        controller.set_brightness(0, brightness as u8);
+        controller.render().unwrap();
+
+        //
+        millis_elapsed += 10;
+        thread::sleep(ten_millis);
+    }
+
+    println!("\nSIGINT/Ctrl+C received, exiting and turning off...");
+
+    // turn off
+    controller.set_brightness(0, 0);
     controller.render().unwrap();
+    controller.wait().unwrap();
+    drop(controller);
 }
---
+++ flash.sh
@@ -0,0 +1,2 @@
+stat target/arm-unknown-linux-gnueabihf/debug/examples/basic | grep Modify
+scp target/arm-unknown-linux-gnueabihf/debug/examples/basic legopi@legopi.local:~/develop/legopi
